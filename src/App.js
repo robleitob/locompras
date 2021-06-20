01d9a417b554e3f8e9234e502b3207174e0c42be
Roberto Leiva <robleitob@gmail.com>
2021-06-19 21:57:43 -0400

    Primer Commit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Bienvenidos, acá tienes de todo lo que necesitas!!!</h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://www.locompras.cl"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LoCompras !!
         </a>
       </header>
     </div>
